efc0a2503c2efce510ea6ede3ac0e352578c0062
mbreaux2023 <mbreaux2023@chadwickschool.org>
2022-01-14 08:39:17 -0800

    Chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mbreaux_DataEncodingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
